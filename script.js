d5b81f8284e6278a67e3e7c2323f272218030b3d
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2024-12-17 00:46:35 +0530

    Page 4 completed

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ document.addEventListener("mousemove", (dets) => {
     blr.style.top = dets.y - 200 + "px"
 })
 
+let h4all = document.querySelectorAll("#nav h4");
+h4all.forEach((elem) => {
+    elem.addEventListener("mouseenter", () => {
+        cur.style.scale = 3;
+        cur.style.border = "1px solid #fff";
+        cur.style.backgroundColor = "transparent";
+    });
+    elem.addEventListener("mouseleave", () => {
+        cur.style.scale = 1;
+        cur.style.border = "0px solid #95C11E";
+        cur.style.backgroundColor = "#95C11E";
+    });
+});
+
 gsap.to("#nav",{
     backgroundColor: "#000",
     duration: 1,
@@ -31,3 +45,68 @@ gsap.to("#main", {
         scrub: 2
     }
 })
+
+gsap.from("#about-us img, .writing", {
+    y:50,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger:{
+        trigger: "#about-us",
+        scoller: "body",
+        start: "top 60%",
+        end: "top 58%",
+        scrub: 3
+    }
+})
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger:{
+        trigger: ".card ",
+        scoller: "body",
+        start: "top 85%",
+        end: "top 65%",
+        scrub: 2
+    }
+})
+
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+
+  gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+      // markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
